674a622dae47ff970881d73576e496315e18b93a
Marshandafitririyani <mfitririyani@gmail.com>
2022-11-13 14:34:03 +0700

    Migrasi 1_2, Rename vrabl

--- app/src/main/java/com/example/roomdatabase/data/Friend.kt
+++ app/src/main/java/com/example/roomdatabase/data/Friend.kt
@@ -3,12 +3,21 @@ package com.example.roomdatabase.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+//@Entity
+//data class Friend(
+//    val name: String,
+//    val school: String,
+//    val hobby: String
+//) {
+//    @PrimaryKey(autoGenerate = true)
+//    var id: Int = 0
+//}
 data class Friend(
-    val name: String,
-    val school: String,
-    val hobby: String
-) {
+    var name: String,
+    var school: String,
+    var hobby: String,
+    var phone: String
+){
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 }
--- app/src/main/java/com/example/roomdatabase/data/MyDatabase.kt
+++ app/src/main/java/com/example/roomdatabase/data/MyDatabase.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 
 @Database(
     entities = [Friend::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class MyDatabase : RoomDatabase() {
@@ -30,10 +32,16 @@ abstract class MyDatabase : RoomDatabase() {
                     MyDatabase::class.java,
                     "data_teman"
                 )
+                    .addMigrations(MIGRATION_1_2)
                     .fallbackToDestructiveMigration()
                     .build()
                 INSTANCE = instance
                 return instance
+            }
+        }
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE user ADD COLUMN phone_number TEXT NOT NULL DEFAULT ''")
             }
         }
     }
